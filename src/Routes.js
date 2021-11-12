8862d7c20a92b102ff51493f4260c2b78d9df479
Adelina <aadelya2702@gmail.com>
2021-11-12 17:40:34 +0600

    add navbar

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter, Routes} from 'react-router-dom'
+import NavBar from './components/Navbar'
+
+const MyRoutes = () => {
+    return (
+      <BrowserRouter>
+     <NavBar>
+
+      <Routes>
+
+      </Routes>
+     </NavBar>
+      </BrowserRouter>
+    );
+};
+
+export default MyRoutes;

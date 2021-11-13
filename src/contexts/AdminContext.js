0caf2c44737e4829b16b208f55bcc04da3ff66bf
Kuba <kubaismailov02@gmail.com>
2021-11-13 14:52:36 +0600

    navbar

---
+++ src/contexts/AdminContext.js
@@ -0,0 +1,25 @@
+import React, { createContext } from 'react';
+
+export const adminContext =  createContext()
+
+const INIT_STATE = {
+
+}
+
+const reducer = (state=INIT_STATE, action) => {
+    switch(action.type){
+        default: 
+            return state;
+    }
+}
+
+
+const AdminContextProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AdminContextProvider;
